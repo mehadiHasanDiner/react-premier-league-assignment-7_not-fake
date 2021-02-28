442cf5d559af4488bb826d5c87bd553258c2b78c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2021-03-01 03:30:35 +0600

    reop created for RPCL

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
+import logo2 from './image/logo2.png'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="col-md-auto">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo2} className="App-logo2" alt="logo" />
+          <h3>React Premier Cricket League (RPCL) </h3>
+
+        </div>
+
+        <nav>
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/budget">Budger</a>
+          <a href="/cricketers">Cricketers</a>
+        </nav>
+        <div className="top-margin d-flex col col-lg">
+        <div className="col col-lg-9"> </div>
+        <div className="col col-lg-3"> </div>
+        </div>
       </header>
     </div>
   );
